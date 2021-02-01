2c134b0a5bc51d7b155ec1f547066ed63febff06
Piyush Kumar <piyushkumar.jiit@gmail.com>
2021-02-01 12:41:12 -0600

    Add files via upload

--- install_hass.sh
+++ install_hass.sh
@@ -4,31 +4,32 @@
 #Abort installation if any of the commands fail
 set -e
 
-PYTHON_VERSION="Python-3.8.0"
+PYTHON_VERSION="3.8.0"
 PYTHON_COMMAND_VERSION="python3.8"
 PYTHON_DOWNLOAD_URL="https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz"
 
 #Update everything
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoclean -y && sudo apt-get autoremove -y
 
+CURRENT_PYTHON_VERSION=`python -c 'import sys; version=sys.version_info[:3]; print("{0}.{1}.{2}".format(*version))'`
 python_version=$(python -V | grep python3.8 > /dev/null 2>&1; echo $?)
-if [[ $python_version -gt "1" ]]
+if [[ $CURRENT_PYTHON_VERSION >= $PYTHON_VERSION ]]
 then
-	echo "$PYTHON_VERSION is not present. Installing."
+	echo "Python-$PYTHON_VERSION is not present. Installing."
 	#Install Python 3.8
 	sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev tar wget vim
 	wget "$PYTHON_DOWNLOAD_URL"
-	sudo tar zxf $PYTHON_VERSION.tgz
-	cd $PYTHON_VERSION
+	sudo tar zxf "Python-$PYTHON_VERSION.tgz"
+	cd "Python-$PYTHON_VERSION"
 	sudo ./configure --enable-optimizations
 	sudo make -j 4
 	sudo make altinstall
-	echo "PYTHON_VERSION version:  $(python3.8 -V)"
+	echo "Updated Python version: $($PYTHON_COMMAND_VERSION -V)"
 	echo "alias python=/usr/local/bin/$PYTHON_COMMAND_VERSION" >> ~/.bashrc
 	source ~/.bashrc
 	echo "Defult python version after update: $(python -V)"
 else
-	echo "$PYTHON_VERSION already available. Proceeding with HA install."
+	echo "Python-$PYTHON_VERSION already available. Proceeding with HA install."
 fi
 
 #Start Home Assistant install
