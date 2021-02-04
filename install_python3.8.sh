618f25e45559d8b9590c0d7d34075304ad5fb30e
Piyush Kumar <piyushkumar.jiit@gmail.com>
2021-02-03 19:43:08 -0600

    Add files via upload

--- install_python3.8.sh
+++ install_python3.8.sh
@@ -18,20 +18,19 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y &
 
 #Install Python
 echo "Installed Python version: $CURRENT_PYTHON_VERSION. $PYTHON_VERSION is not present. Installing."
-#Install REquired libs
-sudo apt-get install -y libtcmalloc-minimal4
+#Install Required libs
+#sudo apt-get install -y libtcmalloc-minimal4
 sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev tar wget vim
-export LD_PRELOAD="/usr/lib/arm-linux-gnueabihf/libtcmalloc_minimal.so.4.5.3"
+
+#export LD_PRELOAD="/usr/lib/arm-linux-gnueabihf/libtcmalloc_minimal.so.4.5.3"
 
 wget "$PYTHON_DOWNLOAD_URL"
-tar -zxf "$PYTHON_VERSION.tgz"
+tar zxf "$PYTHON_VERSION.tgz"
 cd "$PYTHON_VERSION"
 No_Of_Processors=$(cat /proc/cpuinfo|egrep -c "^processor")
 #--prefix=/usr
-sudo ./configure --prefix=/usr/local --enable-optimizations --enable-shared --with-lto --with-system-expat --with-system-ffi --without-ensurepip
-sudo make -j$No_Of_Processors LDFLAGS="-Wl,--strip-all" \
-CFLAGS="-fno-semantic-interposition -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free -ljemalloc" EXTRA_CFLAGS="-DTHREAD_STACK_SIZE=0x100000"
-
+sudo ./configure --enable-optimizations
+sudo make -j$No_Of_Processors
 sudo make altinstall
 #sudo make install
 #sudo update-alternatives --install /usr/bin/python python /usr/local/bin/$PYTHON_COMMAND_VERSION 1
@@ -55,7 +54,7 @@ source ~/.bashrc
 #source /home/homeassistant/.bashrc
 CURRENT_PYTHON_VERSION=`python -c 'import sys; version=sys.version_info[:3]; print("{0}.{1}.{2}".format(*version))'`
 echo "Defult python version after update: $CURRENT_PYTHON_VERSION"
-CURRENT_PYTHON_VERSION=`/usr/local/bin/python3.8.7 -c 'import sys; version=sys.version_info[:3]; print("{0}.{1}.{2}".format(*version))'`
+CURRENT_PYTHON_VERSION=`/usr/local/bin/python3.8 -c 'import sys; version=sys.version_info[:3]; print("{0}.{1}.{2}".format(*version))'`
 echo "New python3 version after update: $CURRENT_PYTHON_VERSION"
 cd ~
 sudo rm -f "$PYTHON_VERSION.tgz"
