f0667994d2e6d934713302d79d9f21a7d26a9321
Piyush Kumar <piyushkumar.jiit@gmail.com>
2021-02-07 00:52:04 -0600

    Add files via upload

--- install_hass_supervised.sh
+++ install_hass_supervised.sh
@@ -28,7 +28,8 @@ do
 done
 
 # Update and upgrade.  Reboot (optional)
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
+sudo apt-get update -q -y && sudo apt-get upgrade -q -y && apt-get dist-upgrade -q -y && sudo apt-get autoremove -q -y
+echo "Update complete."
 
 #Create and Add user to sudo group
 sudo adduser $USER_ACCOUNT
@@ -37,15 +38,50 @@ sudo usermod -aG sudo $USER_ACCOUNT
 if [[ $MODEM_MGR_STATUS != 0 && $DEPENDENCY_STATUS != 0 ]]
 then
 	#Install dependencies
-	sudo apt-get install -y software-properties-common apparmor-utils apt-transport-https ca-certificates curl dbus jq network-manager
+	sudo apt-get -q install -y software-properties-common apparmor-utils apt-transport-https ca-certificates curl dbus jq network-manager
+	echo "Dependencies insta complete."
 	# Disable modem manager
 	sudo systemctl disable ModemManager
 	# Stop modem manager
 	sudo systemctl stop ModemManager
+	sudo systemctl --system daemon-reload
+	echo "ModemManager stopped and disabled."
 
 	echo "Dependencies installed, restarting. Please rerun the script upon restart."
+
+	if [[ $DOCKER_STATUS != 0 ]]
+	then
+		#Install Docker
+		curl -fsSL get.docker.com | sudo /bin/bash -s
+		# Add user to Docker group
+		sudo usermod -aG docker $USER_ACCOUNT
+		sleep 30
+		sudo systemctl --system daemon-reload
+		#Restart Docker
+		sudo systemctl restart docker
+	else
+		echo "Docker already installed. Proceeding with HA installation."
+	fi
+
+	# Install HA Supervised
+	curl -sL "$HA_SUPERVISED_SCRIPT" | sudo /bin/bash -s  -- -m $MACHINE_NAME
+
+	echo ""
+	echo -n "HA starting on $HA_IP_ADDRESS:8123. Waiting ."
+
+	HA_IP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" $HA_IP_ADDRESS:8123)
+
+	while [[ $HA_IP_STATUS != "200"  ]]
+	do
+		HA_IP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" $HA_IP_ADDRESS:8123)
+		sleep 30
+		echo -n "."
+	done
+	echo "."
+	echo "HA UI up @ $HA_IP_ADDRESS:8123. Please proceed with rest of the config using your browser."
+
 	# Reboot
-	sudo reboot
+	#sudo reboot
 else
 	echo "Dependencies set up. Continuing with HA Supervised install."
 	if [[ $DOCKER_STATUS != 0 ]]
