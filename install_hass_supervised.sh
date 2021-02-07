08102d8c0d37532104e7ab1b863a843ae063f03f
Piyush Kumar <piyushkumar.jiit@gmail.com>
2021-02-06 19:22:35 -0600

    Add files via upload

--- install_hass_supervised.sh
+++ install_hass_supervised.sh
@@ -6,7 +6,26 @@ HA_SUPERVISED_SCRIPT="https://raw.githubusercontent.com/Kanga-Who/home-assistant
 USER_ACCOUNT="pi"
 HA_IP_ADDRESS=$(hostname -I | cut -d" " -f 1)
 MODEM_MGR_STATUS=$(sudo systemctl status ModemManager | grep "Active: inactive (dead)" > /dev/null 2>&1; echo $? )
-APPARMOR_STATUS=$( dpkg-query -l | grep apparmor > /dev/null 2>&1; echo $? )
+PACKAGE_STATUS=$( dpkg-query -l | grep apparmor > /dev/null 2>&1; echo $? )
+DEPENDENT_PACKAGES=("software-properties-common" "apparmor-utils" "apt-transport-https" "ca-certificates" "curl" "dbus" "jq" "network-manager")
+
+
+for package in "${DEPENDENT_PACKAGES[@]}"
+do
+  echo "Checking if $package is installed."
+  PACKAGE_STATUS=$( dpkg-query -l | grep $package > /dev/null 2>&1; echo $? )
+  if [[ $PACKAGE_STATUS == 0 ]]
+  then
+  	echo "$package is installed."
+  	let "DEPENDENCY_STATUS = $DEPENDENCY_STATUS + $PACKAGE_STATUS"
+  else
+  	echo "$package is not installed."
+  	# sudo apt-get install -y $package
+  	let "DEPENDENCY_STATUS = $DEPENDENCY_STATUS + $PACKAGE_STATUS"
+  	break
+  fi
+
+done
 
 # Update and upgrade.  Reboot (optional)
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
@@ -15,8 +34,7 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
 sudo adduser $USER_ACCOUNT
 sudo usermod -aG sudo $USER_ACCOUNT
 
-
-if [[ $MODEM_MGR_STATUS != 0 && $APPARMOR_STATUS != 0 ]]
+if [[ $MODEM_MGR_STATUS != 0 && $DEPENDENCY_STATUS != 0 ]]
 then
 	#Install dependencies
 	sudo apt-get install -y software-properties-common apparmor-utils apt-transport-https ca-certificates curl dbus jq network-manager
@@ -65,6 +83,9 @@ else
 	curl -fsSL get.docker.com | sudo /bin/bash
 	# Add user to Docker group
 	sudo usermod -aG docker $USER_ACCOUNT
+	sleep 30
+	#Restart Docker
+	sudo systemctl restart docker
 	#exit
 	# Install HA Supervised
 	curl -sL "$HA_SUPERVISED_SCRIPT" | sudo bash -s  -- -m $MACHINE_NAME
