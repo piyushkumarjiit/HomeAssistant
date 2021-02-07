88c50b29f0085978f3947939bfdc798669d78eab
Piyush Kumar <piyushkumar.jiit@gmail.com>
2021-02-06 19:04:58 -0600

    Add files via upload

--- install_hass_supervised.sh
+++ install_hass_supervised.sh
@@ -12,8 +12,8 @@ APPARMOR_STATUS=$( dpkg-query -l | grep apparmor > /dev/null 2>&1; echo $? )
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
 
 #Create and Add user to sudo group
-adduser $USER_ACCOUNT
-usermod -aG sudo $USER_ACCOUNT
+sudo adduser $USER_ACCOUNT
+sudo usermod -aG sudo $USER_ACCOUNT
 
 
 if [[ $MODEM_MGR_STATUS != 0 && $APPARMOR_STATUS != 0 ]]
@@ -27,7 +27,36 @@ then
 
 	echo "Dependencies installed, restarting. "
 	# Reboot
-	sudo reboot
+	#sudo reboot
+
+	#echo "Dependencies set up. Continuing with HA Supervised install."
+	# Change to root
+	#sudo -i
+	#Install Docker
+	curl -fsSL get.docker.com | sudo /bin/bash -s
+	# Add user to Docker group
+	sudo usermod -aG docker $USER_ACCOUNT
+	sleep 30
+	#Restart Docker
+	sudo systemctl restart docker
+
+	#exit
+	# Install HA Supervised
+	curl -sL "$HA_SUPERVISED_SCRIPT" | sudo /bin/bash -s  -- -m $MACHINE_NAME
+	echo ""
+	echo -n "HA starting on $HA_IP_ADDRESS:8123. Waiting ."
+
+	HA_IP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" $HA_IP_ADDRESS:8123)
+
+	while [[ $HA_IP_STATUS != "200"  ]]
+	do
+		HA_IP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" $HA_IP_ADDRESS:8123)
+		echo -n "."
+		sleep 30
+	done
+
+	echo "HA UI up @ $HA_IP_ADDRESS:8123. Please proceed with rest of the config using your browser."
+
 else
 	echo "Dependencies set up. Continuing with HA Supervised install."
 	# Change to root
